fb4ec812233d1139eafe5adadf07915a938067f4
melinabed <melo36049@hotmail.com>
2023-11-20 11:34:54 -1000

    able to target specific weather info

--- src/city-search.js
+++ src/city-search.js
@@ -1,9 +1,16 @@
 export function citySearch() {
+  //gets user input and displays data
   const userInput = document.getElementById("search").value;
+  //const temp = document.createElement("p");
   fetch(
     `https://api.weatherapi.com/v1/current.json?key=273b38d4a7de4dbbbbb10356231711&q=${userInput}`,
     { mode: "cors" },
-  ).then(function (response) {
-    console.log(response.json());
-  });
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response.current.temp_f);
+      //console.log(response.current.condition.temp_f);
+    });
 }
