86dc65232b4666bf10154b16d2f8bde9e2ccf8fd
melinabed <melo36049@hotmail.com>
2023-11-20 14:56:23 -1000

    added weather info to the document

--- src/city-search.js
+++ src/city-search.js
@@ -1,16 +1,34 @@
-export function citySearch() {
-  //gets user input and displays data
+export async function citySearch() {
+  //Gets the user input and displays the selected info
   const userInput = document.getElementById("search").value;
-  //const temp = document.createElement("p");
-  fetch(
-    `https://api.weatherapi.com/v1/current.json?key=273b38d4a7de4dbbbbb10356231711&q=${userInput}`,
+  const city = document.querySelector(".city");
+  const cityName = document.createElement("h1");
+  const cityTemp = document.createElement("div");
+  const condition = document.createElement("p");
+  const highandLow = document.createElement("div");
+  highandLow.setAttribute("class", "high-low");
+  const high = document.createElement("p");
+  const low = document.createElement("p");
+  city.appendChild(cityName);
+  city.appendChild(cityTemp);
+  city.appendChild(condition);
+  city.appendChild(highandLow);
+  highandLow.appendChild(high);
+  highandLow.appendChild(low);
+
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=273b38d4a7de4dbbbbb10356231711&q=${userInput}&days=1&aqi=no&alerts=no`,
     { mode: "cors" },
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response.current.temp_f);
-      //console.log(response.current.condition.temp_f);
-    });
+  );
+  const weatherInfo = await response.json();
+  console.log(weatherInfo);
+  cityName.textContent = weatherInfo.location.name;
+  cityTemp.textContent = weatherInfo.current.temp_f + "\u00B0";
+  condition.textContent = weatherInfo.current.condition.text;
+  high.textContent = `H: ${
+    weatherInfo.forecast.forecastday[0].day.maxtemp_f + "\u00B0"
+  }`;
+  low.textContent = `L: ${
+    weatherInfo.forecast.forecastday[0].day.mintemp_f + "\u00B0"
+  }`;
 }
